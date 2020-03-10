8f0acc14b00ad453986ad9a4eab2754448c71267
yt8492 <tmymyut2000@gmail.com>
2020-03-10 22:23:32 +0900

    fix main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,8 @@
 import org.w3c.dom.HTMLButtonElement
 import org.w3c.dom.HTMLHeadingElement
 import kotlin.browser.document
-import kotlin.browser.window
 
 fun main() {
-    window.addEventListener("load", {
-        onLoadWindow()
-    })
-}
-
-fun onLoadWindow() {
     val greetButton = document.getElementById("greetButton") as HTMLButtonElement
     val greetText = document.getElementById("greetText") as HTMLHeadingElement
     greetButton.addEventListener("click", {
